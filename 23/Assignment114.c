42977306ef6efcf7dfabf8dae7f99acee0c098b9
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2024-12-18 23:14:46 +0530

    Add files via upload

---
+++ 23/Assignment114.c
@@ -0,0 +1,56 @@
+/*
+Accept the Number of Rows and column from the user and display below pattern    
+Example :
+Input   :   iRow = 6    iCol = 5
+Output  :   
+
+        *   *   *   *   *   
+        *   @   @   @   *
+        *   @   @   @   *
+        *   @   @   @   *
+        *   @   @   @   *
+        *   *   *   *   * 
+        
+   
+*/
+
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <=iRow;i++)
+    {
+        for(j=1;j<=iCol;j++)
+        {
+            if( i == 1 || i == iRow || j == 1 || j == iCol )
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("@\t");
+            }
+        }
+
+        printf("\n");
+    }
+    
+}
+
+
+int main()
+{   
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the Number of Rows :\n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the Number of Columns :\n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
